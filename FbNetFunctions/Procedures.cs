709eb35a63d1278f9a6c39375616466c6ca1d849
Rodrigo Tessari <rodrigo.tessari@outlook.com>
2018-07-10 12:54:33 -0300

    Add project

---
+++ FbNetFunctions/Procedures.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+
+namespace FbNetFunctions
+{
+    public static class Procedures
+    {
+        public static IEnumerator<(string, string, string)> DownloadFile(string s, string n)
+        {
+            var root = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ProgramFiles), "Automatiza", "QrCodes");
+            Directory.CreateDirectory(root);
+
+            var d = Path.Combine(root, n);
+            var client = new WebClient();
+            client.DownloadFile(s, d);
+
+            yield return (s, n, d);
+        }
+
+        public static IEnumerator<(string, byte[])> DownloadData(string s)
+        {
+            var client = new WebClient();
+            var data = client.DownloadData(s);
+
+            yield return (s, data);
+        }
+    }
+}
---
+++ sql/script.sql
@@ -0,0 +1,9 @@
+recreate procedure download_file(s varchar(4000), n varchar(200))
+returns (original_s varchar(4000), original_n varchar(4000),new_d varchar(4000))
+external name 'FbNetFunctions!FbNetFunctions.Procedures.DownloadFile'
+engine FbNetExternalEngine;
+
+recreate procedure download_data(s varchar(4000))
+returns (original_s varchar(4000), new_d varchar(4000))
+external name 'FbNetFunctions!FbNetFunctions.Procedures.DownloadData'
+engine FbNetExternalEngine;
